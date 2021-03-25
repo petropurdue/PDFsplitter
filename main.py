b8e8b5033cb6eee3e89706fe3c5e6a53993688b9
ziro <petrop@purdue.edu>
2021-03-24 22:54:27 -0400

    Added comments for clarity

--- main.py
+++ main.py
@@ -2,12 +2,19 @@ import PyPDF4 as pdf
 import PyPDF2
 import glob
 
-def pdfsplit(filename):
+#Optional improvements that could be made to this program:
+#Don't run if a pdf can't be found
+#Don't run if the pdf length is 1
+
+def pdfsplit(listlbl, filename): #this is where the splitting actually happens
+    #Initializations
     pdfFileObj = open(filename, 'rb')
     pdfReader = pdf.PdfFileReader(pdfFileObj)
     pagereader = PyPDF2.PdfFileReader(filename)
+
+    #Page splitting
     for i in range(pdfReader.numPages):
-        writefilename = chr(i+49) + '.pdf'
+        writefilename = listlbl + chr(i+49) + '.pdf'    #write as a1.pdf, a2.pdf, b1.pdf, b2.pdf, etc
         pdf_writer = PyPDF2.PdfFileWriter()
         pdf_writer.addPage(pagereader.getPage(i))
         with open(writefilename, 'wb') as output_file:
@@ -15,9 +22,9 @@ def pdfsplit(filename):
         #print(i)
     return
 
-def getfilenames(): #put the pdf file in the folder where you run this, it'll take care of the rest for you
+def getfilenames(): #put the pdf file in the folder where you run this, it'll take care of the rest for you. You can name it whatever non-integer you want.
     fnlist = []
-    for file in glob.glob("[A-z][A-z]*.pdf"):
+    for file in glob.glob("[A-z][A-z]*.pdf"): #get all the pdf files that are not outputs from this program and split them
         fnlist.append(file)
         #print(file)
     print("Slicing",fnlist)
@@ -29,4 +36,4 @@ if __name__ == '__main__':
     for a in range(len(fnl)):
         listlabel = (chr(a+97))
         pdfsplit(listlabel,fnl[a])
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print("PDF split successfully!")
